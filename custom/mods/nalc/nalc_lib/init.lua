5a47a0834753dab151f3d3e9e60b52cd676a07e9
sys4-fr <sys4@free.fr>
2018-03-26 19:36:46 +0200

    Update nalc_lib mod

--- custom/mods/nalc/nalc_lib/init.lua
+++ custom/mods/nalc/nalc_lib/init.lua
@@ -22,6 +22,27 @@ local function remove_group(name, group)
 	end
 end
 
+-- Add node to group
+local function add_group(name, group, value)
+	local node = minetest.registered_nodes[name]
+
+	if node then
+		local groups = node.groups
+		if not groups then
+			groups = {}
+		end
+		groups[group] = value
+
+		minetest.log("action", "[nalc_lib] Add group "..group.."="..value.." to "..name)
+		minetest.override_item(name, {groups = groups})
+	end
+end
+
+-- Add a node in xp group
+function nalc.def_xp(name, value)
+	add_group(name, "xp", value)
+end
+
 -- Remove node from group "oddly_breakable_by_hand"
 function nalc.not_hand_breakable(name)
 	remove_group(name, "oddly_breakable_by_hand")
